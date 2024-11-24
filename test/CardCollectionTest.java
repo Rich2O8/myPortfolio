d18ca33a4cb1d8bcc594f106ae8767ffd6127157
Rich2O8 <tinocolopez.2@osu.edu>
2024-11-24 00:17:42 -0500

    Test cases for secondary methods.

--- test/CardCollectionTest.java
+++ test/CardCollectionTest.java
@@ -442,4 +442,205 @@ public class CardCollectionTest {
         org.junit.Assert.assertEquals(expectedName, cardNameClear);
         org.junit.Assert.assertEquals(expectedSize, mapSizeAfter);
     }
+
+    /**
+     * Testing transferFrom on a card with map size 0.
+     */
+    @Test
+    public void testTransferfrom() {
+        CardCollectionMap darkMagician = new CardCollectionMap("Dark Magician");
+        CardCollectionMap pikachu = new CardCollectionMap("Pikachu");
+
+        darkMagician.addCardValue("11/19/2004", 0);
+
+        darkMagician.transferFrom(pikachu);
+        String expectedName = "Pikachu", cardName = darkMagician.card();
+        int dMagiZiseAfter = darkMagician.mapSize(), dMagiExpctSize = 1;
+        String pikachuAfter = pikachu.card(), pikachuExpctAfter = "";
+        int pikachuSizeAfter = pikachu.mapSize(), pikachuExpctSize = 0;
+
+        org.junit.Assert.assertEquals(expectedName, cardName);
+        org.junit.Assert.assertEquals(dMagiExpctSize, dMagiZiseAfter);
+
+        org.junit.Assert.assertEquals(pikachuExpctAfter, pikachuAfter);
+        org.junit.Assert.assertEquals(pikachuExpctSize, pikachuSizeAfter);
+    }
+
+    /**
+     * Testing transferFrom on two cards of size 1.
+     */
+    @Test
+    public void testTransferfrom1() {
+        CardCollectionMap darkMagician = new CardCollectionMap("Dark Magician");
+        CardCollectionMap pikachu = new CardCollectionMap("Pikachu");
+
+        darkMagician.addCardValue("11/19/2004", 0);
+        pikachu.addCardValue("12/15/2005", 1);
+
+        darkMagician.transferFrom(pikachu);
+        String expectedName = "Pikachu", cardName = darkMagician.card();
+        int dMagiZiseAfter = darkMagician.mapSize(), dMagiExpctSize = 2;
+
+        String pikachuAfter = pikachu.card(), pikachuExpctAfter = "";
+        int pikachuSizeAfter = pikachu.mapSize(), pikachuExpctSize = 0;
+
+        org.junit.Assert.assertEquals(expectedName, cardName);
+        org.junit.Assert.assertEquals(dMagiExpctSize, dMagiZiseAfter);
+
+        org.junit.Assert.assertEquals(pikachuExpctAfter, pikachuAfter);
+        org.junit.Assert.assertEquals(pikachuExpctSize, pikachuSizeAfter);
+    }
+
+    /**
+     * Testing newInstace.
+     */
+    @Test
+    public void testNewInstance() {
+        CardCollectionMap ddd = new CardCollectionMap("D/D/D Ceaser");
+        CardCollectionMap newCard = ddd.newInstance();
+
+        String newCardName = newCard.card(), newCardExpctName = "D/D/D Ceaser";
+        org.junit.Assert.assertEquals(newCardExpctName, newCardName);
+    }
+
+    //Testing Secondary Methods------------------------------------------------
+
+    /**
+     * Testing valueBWDates on a card with two recorded values.
+     */
+    @Test
+    public void testValueBWDates() {
+        String blueDraco = "Blue Eyes White Dragon";
+        CardCollectionMap blue = new CardCollectionMap(blueDraco);
+
+        blue.addCardValue("10/01/2021", 1);
+        blue.addCardValue("10/01/2023", 2);
+
+        int blSize = blue.mapSize(), blSizeExpected = 2;
+        int between21And23 = blue.valueBWDates("10/01/2021", "10/01/2023");
+        int expectedValueBetween = 1;
+
+        org.junit.Assert.assertEquals(blSizeExpected, blSize);
+        org.junit.Assert.assertEquals(expectedValueBetween, between21And23);
+    }
+
+    /**
+     * Testing valueBWDates on a card with date1 value > date2 value.
+     */
+    @Test
+    public void testValueBWDates1() {
+        String blueDraco = "Blue Eyes White Dragon";
+        CardCollectionMap blue = new CardCollectionMap(blueDraco);
+
+        blue.addCardValue("10/01/2021", 50);
+        blue.addCardValue("10/01/2023", 2);
+
+        int blSize = blue.mapSize(), blSizeExpected = 2;
+        int between21And23 = blue.valueBWDates("10/01/2021", "10/01/2023");
+        int expectedValueBetween = -48;
+
+        org.junit.Assert.assertEquals(blSizeExpected, blSize);
+        org.junit.Assert.assertEquals(expectedValueBetween, between21And23);
+    }
+
+    /**
+     * Testing changeDate on a date, value pair of a card.
+     */
+    @Test
+    public void testChangeDate() {
+        String blueDraco = "Blue Eyes White Dragon";
+        CardCollectionMap blue = new CardCollectionMap(blueDraco);
+
+        blue.addCardValue("10/01/2021", 1);
+        blue.addCardValue("10/01/2023", 2);
+
+        blue.changeDate("10/01/2021", "08/05/2021");
+
+        int expectedValueOfChangedDate = 1,
+                valueOfChangedDate = blue.valAtDate("08/05/2021");
+        int blSize = blue.mapSize(), blSizeExpected = 2;
+
+        org.junit.Assert.assertEquals(blSizeExpected, blSize);
+        org.junit.Assert.assertEquals(expectedValueOfChangedDate,
+                valueOfChangedDate);
+    }
+
+    /**
+     * Testing changeDate on a date, value pair of a card.
+     */
+    @Test
+    public void testChangeDate1() {
+        String blueDraco = "Blue Eyes White Dragon";
+        CardCollectionMap blue = new CardCollectionMap(blueDraco);
+
+        blue.addCardValue("10/01/2021", 1);
+        blue.addCardValue("10/01/2023", 25);
+
+        blue.changeDate("10/01/2023", "10/01/2022");
+
+        int expectedValueOfChangedDate = 25,
+                valueOfChangedDate = blue.valAtDate("08/05/2021");
+        int blSize = blue.mapSize(), blSizeExpected = 2;
+
+        org.junit.Assert.assertEquals(blSizeExpected, blSize);
+        org.junit.Assert.assertEquals(expectedValueOfChangedDate,
+                valueOfChangedDate);
+    }
+
+    /**
+     * Testing valBigger on card with 2 records, 1st date is smaller than 2nd,
+     * should result in false.
+     */
+    @Test
+    public void testValBigger() {
+        String blueDraco = "Blue Eyes White Dragon";
+        CardCollectionMap blue = new CardCollectionMap(blueDraco);
+
+        String twenty1 = "10/01/2021", twenty3 = "10/01/2023";
+        blue.addCardValue(twenty1, 1);
+        blue.addCardValue(twenty3, 2);
+
+        boolean twentyOneBigger23 = blue.valBigger(twenty1, twenty3);
+        boolean twentyOneNuhUh = false;
+
+        org.junit.Assert.assertEquals(twentyOneNuhUh, twentyOneBigger23);
+    }
+
+    /**
+     * Testing valBigger on card with 2 records,they have the same value. Should
+     * result in false.
+     */
+    @Test
+    public void testValBigger1() {
+        String blueDraco = "Blue Eyes White Dragon";
+        CardCollectionMap blue = new CardCollectionMap(blueDraco);
+
+        String twenty1 = "10/01/2021", twenty3 = "10/01/2023";
+        blue.addCardValue(twenty1, 1);
+        blue.addCardValue(twenty3, 1);
+
+        boolean twentyOneBigger23 = blue.valBigger(twenty1, twenty3);
+        boolean twentyOneNuhUh = false;
+
+        org.junit.Assert.assertEquals(twentyOneNuhUh, twentyOneBigger23);
+    }
+
+    /**
+     * Testing valBigger on card with 2 records,1st date > 2nd date value.
+     * Should result in true.
+     */
+    @Test
+    public void testValBigger2() {
+        String blueDraco = "Blue Eyes White Dragon";
+        CardCollectionMap blue = new CardCollectionMap(blueDraco);
+
+        String twenty1 = "10/01/2021", twenty3 = "10/01/2023";
+        blue.addCardValue(twenty1, 2);
+        blue.addCardValue(twenty3, 0);
+
+        boolean twentyOneBigger23 = blue.valBigger(twenty1, twenty3);
+        boolean twentyOneYes = true;
+
+        org.junit.Assert.assertEquals(twentyOneYes, twentyOneBigger23);
+    }
 }
