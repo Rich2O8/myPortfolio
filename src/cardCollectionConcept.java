c7c8ed27b241e446f657ab002dd59e90f9e86737
Rich2O8 <tinocolopez.2@osu.edu>
2024-11-25 10:45:12 -0500

    Return cardCollectionConcept to original state.

--- src/cardCollectionConcept.java
+++ src/cardCollectionConcept.java
@@ -1,41 +1,62 @@
-import components.cardcollection.CardCollectionMap;
-
+import java.util.HashMap;
+/**
+ * Concept of component, very basic.
+ */
 public class CardCollectionConcept {
-    // private String card;
-    // private HashMap<String, Integer> cardValues;
-
-    // public CardCollectionConcept(String cardName) {
-    //     this.card = cardName;
-    //     this.cardValues = new HashMap<String, Integer>();
-    // }
-
-    // public String getCard() {
-    //     return this.card;
-    // }
-
-    // public void setDateValue(String date, int value) {
-    //     this.cardValues.put(date, value);
-    // }
-
-    // public int getValueAtDate(String date) {
-    //     return this.cardValues.get(date);
-    // }
+    /** name of card. */
+    private String card;
+    /** Map where values are stored. */
+    private HashMap<String, Integer> cardValues;
 
     /**
-     * testing
+     * Constructor.
+     * @param cardName
+     */
+    public CardCollectionConcept(String cardName) {
+        this.card = cardName;
+        this.cardValues = new HashMap<String, Integer>();
+    }
+
+    /**
+     * gets card name.
+     * @return card name
+     */
+    public String getCard() {
+        return this.card;
+    }
+
+    /**
+     * Adds date,value pair to cardvalues.
+     * @param date
+     * @param value
+     */
+    public void setDateValue(String date, int value) {
+        this.cardValues.put(date, value);
+    }
+
+    /**
+     * Gets the value at {@code date}.
+     * @param date
+     * @return value at date
+     */
+    public int getValueAtDate(String date) {
+        return this.cardValues.get(date);
+    }
+
+    /**
+     * testing.
      *
      * @param args
      */
     public static void main(String[] args) {
-        CardCollectionMap pika = new CardCollectionMap("Pikachu");
+        CardCollectionConcept pika = new CardCollectionConcept("Pikachu");
 
-        pika.addCardValue("10/21/2024", 1500);
-        pika.addCardValue("11/14/2024", 5);
+        pika.setDateValue("10/21/2024", 1500);
+        pika.setDateValue("11/14/2024", 5);
 
-        System.out.println("This is the rare " + pika.card() + " card!");
+        System.out.println("This is the rare " + pika.getCard() + " card!");
         System.out.println(
-                "The value of " + pika.card() + " at 10/21/2024 was: ");
-        System.out.println("$ " + pika.valAtDate("10/21/2024"));
-
+                "The value of " + pika.getCard() + " at 10/21/2024 was: ");
+        System.out.println("$ " + pika.getValueAtDate("10/21/2024"));
     }
 }
